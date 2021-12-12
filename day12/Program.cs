a7a53b058a0d72e3954c741dc6e170026547cc83
Thomas Kim <tkim@varicent.com>
2021-12-12 00:51:41 -0600

    day 12.
    this took a long time to do.
    
    struggled on impelmentation.
    
    Tried BFS first. Wrong approach. Had an interative solution.
    
    Implemented DFS instead using recursion. this took over an hour.
    
    Part 2 had a relatively simple solution but had 1 problems.
    
    1. i was getting confused what the skip condition should be. you can visit a small cave at most twice and all others once.  decided to use a double_dip flag and pass it around.
    2. a bug where i wasn't inheriting double_dip through recursion.
    
    This one took much longer than the rest so far.

--- day12/Program.cs
+++ day12/Program.cs
@@ -9,13 +9,92 @@ namespace day9
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> node_idx = new Dictionary<string, int>();
+            Dictionary<int, string> node_name = new Dictionary<int, string>();
+            var edges = new List<string>();
+
             using (var reader = File.OpenText("input.txt"))
             {
+                var idx = 0;
                 while (reader.Peek() > -1)
                 {
-		}
-	    }
- 
+
+                    var input = reader.ReadLine();
+                    edges.Add(input);
+                    var nodes = input.Split('-');
+                    
+                    if (!node_idx.ContainsKey(nodes[0])) {
+                        node_idx.Add(nodes[0], idx);
+                        node_name.Add(idx, nodes[0]);
+                        idx++;
+                    }
+
+                    if (!node_idx.ContainsKey(nodes[1])) {
+                        node_idx.Add(nodes[1], idx);
+                        node_name.Add(idx, nodes[1]);
+                        idx++;
+                    }
+
+                }
+            }
+
+            bool[][] matrix = new bool[node_idx.Keys.Count][];
+            for (int i=0 ; i<matrix.Length ; i++) 
+                matrix[i] = new bool[node_idx.Keys.Count];
+
+            foreach (var edge in edges) {
+                var e = edge.Split('-');
+
+                matrix[node_idx[e[0]]][node_idx[e[1]]] = true;
+                matrix[node_idx[e[1]]][node_idx[e[0]]] = true;
+            }
+
+            var paths = findEnd(matrix, "start", node_idx, node_name, new List<string>());
+            Console.WriteLine(paths);
         }
+        
+        static int findEnd(bool[][] matrix, string node, 
+                            Dictionary<string, int> node_idx, 
+                            Dictionary<int, string> node_name,
+                            List<string> banned,
+                            string path = "",
+                            bool double_dip = false) {
+                
+                path += "," + node;
+                if (node == "end") {
+                    //Console.WriteLine(path);
+                    return 1;
+                }
+
+                var ni = node_idx[node];
+                var result = 0;
+
+                var dd = double_dip;
+
+                if (node_name[ni][0] > (int)'Z' && 
+                    node_name[ni] != "start" && 
+                    node_name[ni] != "end") {
+                    
+                    if (banned.Contains(node_name[ni])) {
+                        if (double_dip)
+                            return 0;
+
+                        dd = true;
+                    }
+                    else 
+                        banned.Add(node_name[ni]);
+                }
+
+                for (int i=0 ; i<matrix.Length ; i++) {
+                    if (matrix[ni][i] && 
+                        node_name[i] != "start")
+                    {
+                        result += findEnd(matrix, node_name[i], node_idx, node_name, new List<string>(banned), path, dd);
+                        }
+                }
+
+                return result;
+            }
     }
+
 }
